a3ee26e16d64515ef1c50539a92523d613dc29f7
Timur <arbo1234@yandex.ru>
2022-08-13 19:01:32 +0300

    Добавьте файлы проекта.

---
+++ less1/HomeWork/task2.py
@@ -0,0 +1,9 @@
+﻿#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+x = int(input("Введите X "))
+y = int(input("Введите Y "))
+z = int(input("Введите Z "))
+left = not (x or y or z)
+right = not x and not y and not z
+result = left == right
+if result == True : print("Истино")
+else : print ("Лож")
